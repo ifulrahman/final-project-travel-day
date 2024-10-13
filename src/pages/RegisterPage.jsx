8175c7abee76dae4863d5f8e5c776dea3a2697a7
ifulrahman <syaifulmuhammad02@gmail.com>
2024-10-13 20:20:16 +0700

    Update RegisterPage.jsx
    Fixing Redux on RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import tdLogo from '../assets/td-logo.png';
 import nusaPenida from '../assets/nusa-penida.jpg';
+import { loginSuccess } from '../redux/slices/authSlice';
 
 const RegisterPage = () => {
   const [details, setDetails] = useState({
